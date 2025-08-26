b63acf66e04284566d0633b29db5face6956a257
Filip Markovic <filip98122@gmail.com>
2025-08-26 13:52:17 +0200

    King part

--- king.py
+++ king.py
@@ -4,6 +4,15 @@ class king:
         s.x=x
         s.y=y
         s.color=color
-        s.moveopt=moveopt
-    def calc_move_opt():
+        if s.color=="b":
+            s.oppositecolor="w"
+        else:
+            s.oppositecolor="b"
+        s.moveopt=[]
+        s.spaces=[[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0]]
+    def calc_move_opt(s,map):
         
+        current=map[s.y][s.x]
+        for i in range(len(s.spaces)):
+            if map[s.y+s.spaces[i][1]][s.x+s.spaces[i][0]]=="" or map[s.y+s.spaces[i][1]][s.x+s.spaces[i][0]][1]==s.oppositecolor:
+                s.moveopt.append([s.y+s.spaces[i][1],s.x+s.spaces[i][0]])
